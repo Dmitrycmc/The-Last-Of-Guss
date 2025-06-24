5eaa7b65b9df63e0eb3a568e003c46681b738ad3
Dmitrii Lytov <lytovdmitriidev@gmail.com>
2025-06-24 05:21:21 +0300

    Fixed nikita role

--- backend/src/utils/roles.ts
+++ backend/src/utils/roles.ts
@@ -4,7 +4,7 @@ export const resolveRole = (username: string): Role => {
     if (username === 'admin') {
         return Role.ADMIN_ROLE
     }
-    if (username === 'Никита') {
+    if (username === 'nikita') {
         return Role.NIKITA_ROLE
     }
     return Role.USER_ROLE
